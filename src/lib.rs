e8ca7911afd5951b73395f5c0d583f2d85b5362c
sunnywhy <sunnywhy@gmail.com>
2021-12-31 23:51:41 -0500

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
-use std::{path::Path, io::{BufReader, Stdout, self, Write, Read, BufRead}, fs::File, ops::Range};
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader, Read, Stdout, Write},
+    ops::Range,
+    path::Path,
+};
 
 use clap::Parser;
 use colored::*;
@@ -8,7 +13,6 @@ use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use regex::Regex;
 
 mod error;
-
 
 /// define a Type, to make it simpler
 pub type StrategyFn<W, R> = fn(&Path, BufReader<R>, &Regex, &mut W) -> Result<(), GrepError>;
@@ -29,7 +33,7 @@ impl GrepConfig {
 
     pub fn match_with(&self, strategy: StrategyFn<Stdout, File>) -> Result<(), GrepError> {
         let regex = Regex::new(&self.pattern)?;
-        // geenerate the file list 
+        // geenerate the file list
         let files: Vec<_> = glob::glob(&self.glob)?.collect();
         // hanlde files in parallel
         files.into_par_iter().for_each(|v| {
@@ -55,11 +59,20 @@ pub fn default_strategy<W: Write, R: Read>(
     pattern: &Regex,
     writer: &mut W,
 ) -> Result<(), GrepError> {
-    let matches: String = reader.lines().enumerate().map(|(lineno, line)| {
-       line.ok().map(|line| {
-           pattern.find(&line).map(|m| format_line(&line, lineno+1, m.range()))
-       }).flatten()
-    }).filter_map(|v| v.ok_or(()).ok()).join("\n");
+    let matches: String = reader
+        .lines()
+        .enumerate()
+        .map(|(lineno, line)| {
+            line.ok()
+                .map(|line| {
+                    pattern
+                        .find(&line)
+                        .map(|m| format_line(&line, lineno + 1, m.range()))
+                })
+                .flatten()
+        })
+        .filter_map(|v| v.ok_or(()).ok())
+        .join("\n");
 
     if !matches.is_empty() {
         writer.write(path.display().to_string().green().as_bytes())?;
@@ -72,14 +85,15 @@ pub fn default_strategy<W: Write, R: Read>(
 }
 
 fn format_line(line: &str, lineno: usize, range: Range<usize>) -> String {
-   let Range {start, end} = range;
-   let prefix = &line[..start];
-   format!("{0: >6}:{1: <3} {2}{3}{4}", 
-   lineno.to_string().blue(), 
-   // for non-ascii characters, we cannot use prefix.len(), here is an O(n) operation, only for demo purpose now
-   (prefix.chars().count() + 1).to_string().cyan(), 
-   prefix,
-   &line[start..end].red(),
-   &line[end..]
-)
+    let Range { start, end } = range;
+    let prefix = &line[..start];
+    format!(
+        "{0: >6}:{1: <3} {2}{3}{4}",
+        lineno.to_string().blue(),
+        // for non-ascii characters, we cannot use prefix.len(), here is an O(n) operation, only for demo purpose now
+        (prefix.chars().count() + 1).to_string().cyan(),
+        prefix,
+        &line[start..end].red(),
+        &line[end..]
+    )
 }
